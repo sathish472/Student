8af89bf2fd7c3f0f58ca114bc3c4b8e368f5fb0d
Satish <sathish.m99@gmail.com>
2024-07-05 14:38:36 +0530

    17

--- src/main/java/com/student/entity/Student.java
+++ src/main/java/com/student/entity/Student.java
@@ -1,10 +1,11 @@
 package com.student.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
